4d9efa2a0da2d6a0f4a3585ff4f3d539e0156180
FilipeChaves <filipe_andrebc@hotmail.com>
2016-10-05 17:00:01 +0100

    fix countdown

--- js/main.js
+++ js/main.js
@@ -310,11 +310,19 @@ function onLoadedPage(){
 }
 
 function getTimeRemaining(){
-  var t = new Date(2017, 8, 22, 0, 0, 0, 0) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
+  var t = new Date(2017, 7, 22, 21, 0, 0, 0) - new Date();
+  if(t < 0){
+	  seconds = 0;
+	  minutes = 0;
+	  hours = 0;
+	  days = 0;
+  }
+  else{
+	  var seconds = Math.floor( (t/1000) % 60 );
+	  var minutes = Math.floor( (t/1000/60) % 60 );
+	  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	  var days = Math.floor( t/(1000*60*60*24) );
+  }
   return {
     'total': t,
     'days': days,
