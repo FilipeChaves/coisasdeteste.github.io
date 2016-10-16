cd40c792367b8bcec9f2d7b94e42809b17aca04f
FilipeChaves <filipe_andrebc@hotmail.com>
2016-10-16 20:48:43 +0100

    fix spaces and cross location

--- js/main.js
+++ js/main.js
@@ -392,7 +392,7 @@ function SetTopAndHeight(nextID, titleName, textName)
 	
 	if(nextID > 25){
 		$('div.artistsCross').css('display', 'inline-block');
-		$('div.artistsCross').css('top', countdownHeight + 40 + "px");
+		$('div.artistsCross').css('top', countdownHeight + 30 + "px");
 		$(textName).css('top', countdownHeight + 40 + "px");
 		$(textName).css('height', (windowHeight - 60 - countdownHeight - menuSocialHeight) + "px");
 	}
