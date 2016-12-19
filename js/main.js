b47e8cb4375e40f2f6ab7f5742afb55cc3c16012
FilipeChaves <filipe_andrebc@hotmail.com>
2016-12-19 23:41:30 +0000

    new artist

--- js/main.js
+++ js/main.js
@@ -469,6 +469,15 @@ function antaroClick(){
 	window.history.pushState({"href":"antaro"}, null, '#antaro');
 }
 
+function fluxClick(){
+	removeCenter(49);
+	removeActives();
+	
+	$('div.program').addClass('program-active');
+	fadeout();
+	window.history.pushState({"href":"flux"}, null, '#flux');
+}
+
 function removeCenter(nextID)
 {
 	var allTitleAreas = ['div.titulo-concept',
@@ -519,7 +528,8 @@ function removeCenter(nextID)
 						 'div.titulo-xenomorph',
 						 'div.titulo-atmos',
 						 'div.titulo-magus',
-						 'div.titulo-antaro'];
+						 'div.titulo-antaro',
+						 'div.titulo-flux'];
 						
 	var allTextAreas = ['div.textArea-concept', 
 						'div.textArea-program',
@@ -569,7 +579,8 @@ function removeCenter(nextID)
 						'div.textArea-xenomorph',
 						'div.textArea-atmos',
 						'div.textArea-magus',
-						'div.textArea-antaro'];
+						'div.textArea-antaro',
+						'div.textArea-flux'];
 	
 	if(nextID <= 25)
 		$('div.artistsCross').css('opacity', '0');
@@ -683,7 +694,8 @@ function removeDisplay(nextID)
 						 'div.titulo-xenomorph',
 						 'div.titulo-atmos',
 						 'div.titulo-magus',
-						 'div.titulo-antaro'];
+						 'div.titulo-antaro',
+						 'div.titulo-flux'];
 						
 	var allTextAreas = ['div.textArea-concept', 
 						'div.textArea-program',
@@ -733,7 +745,8 @@ function removeDisplay(nextID)
 						'div.textArea-xenomorph',
 						'div.textArea-atmos',
 						'div.textArea-magus',
-						'div.textArea-antaro'];
+						'div.textArea-antaro',
+						'div.textArea-flux'];
 	
 	var contdownHeight = $('div.countdown').height();
 	var titleHeight = $('div.titulo-concept').height();
@@ -979,6 +992,7 @@ function onLoadedPage(){
 	var nicesxs = $("div.textArea-xenomorph").niceScroll({touchbehavior:false,cursorcolor:"#FFFFF",cursoropacitymax:0.6,cursorwidth:8,autohidemode:false});
 	var nicesxs = $("div.textArea-atmos").niceScroll({touchbehavior:false,cursorcolor:"#FFFFF",cursoropacitymax:0.6,cursorwidth:8,autohidemode:false});	
 	var nicesxs = $("div.textArea-antaro").niceScroll({touchbehavior:false,cursorcolor:"#FFFFF",cursoropacitymax:0.6,cursorwidth:8,autohidemode:false});	
+	var nicesxs = $("div.textArea-flux").niceScroll({touchbehavior:false,cursorcolor:"#FFFFF",cursoropacitymax:0.6,cursorwidth:8,autohidemode:false});	
 	
 	$('div.textArea-buy').append("<div id=\"tkt-content\" style=\"width:96%; height:500px\"></div><img src=\"https://www.ticketea.pt/images/powered_by.png\" alt=\"ticketea\" />" +
 				"<p class=\"texto\"> In case you need assistance please contact us through the following email - <a href=\"mailto:tickets@znagathering.com\">tickets@znagathering.com</a>.</p>");
@@ -1228,6 +1242,9 @@ function clickInCorrectTab(name){
 			break;
 		case 'antaro':
 			antaroClick();
+			break;
+		case 'flux':
+			fluxClick();
 			break;
 	}
 }
